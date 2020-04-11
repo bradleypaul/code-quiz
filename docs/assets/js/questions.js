4127168695ccb4213dd06df029fd5a08fb4d7944
Paul Bradley <bradleypauld@gmail.com>
2020-04-11 09:35:00 -0500

    finished challenge

---
+++ docs/assets/js/questions.js
@@ -0,0 +1,32 @@
+const questions = [
+    {
+        q: "Arrays in JavaScript can contain which types?",
+        ans: [
+            "numbers and strings",
+            "objects",
+            "other arrays",
+            "all of the above"
+        ],
+        correctAnswer: 'all of the above'
+    },
+    {
+        q: "Commonly used data types in JavaScript do not include:",
+        ans: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"
+        ],
+        correctAnswer: "alerts"
+    },
+    {
+        q: "The conditional statements in an if/else block are contained in:",
+        ans: [
+            "parenthesis",
+            "curly braces",
+            "angle brackets",
+            "square brackets"
+        ],
+        correctAnswer: "curly braces"
+    }
+];
